776bef690c5202f500eadc0f839094ea50a1488c
swayam-pradhan <swayampradhan7205@gmail.com>
2025-01-20 01:24:40 +0530

    Update main.py
    added advanced features

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pyttsx3
 from datetime import datetime
+from random import random
 import speech_recognition as sr
 import wikipedia
+import requests
 import webbrowser
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-
 
 def speak(audio):
     engine.say(audio)
@@ -42,41 +43,118 @@ def takeCommand():
         return "None"
     return query
 
+def searchGoogle(query):
+    """Perform a Google search."""
+    speak(f"Searching Google for {search_query}")
+    webbrowser.open(f"https://www.google.com/search?q={search_query}")
+
+
+def getWeather():
+    """Fetch weather information."""
+    speak("Which city's weather would you like to know?")
+    city = takeCommand().lower()
+    api_key = 'YOUR_OPENWEATHERMAP_API_KEY'  # Replace with your API key
+    base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    # Make a request to OpenWeatherMap API
+    try:
+        response = requests.get(base_url)
+        data = response.json()
+
+        if data["cod"] != "404":
+            main_data = data["main"]
+            weather_data = data["weather"][0]
+            temperature = main_data["temp"]
+            humidity = main_data["humidity"]
+            description = weather_data["description"]
+
+            # Provide weather information
+            weather_report = (f"The temperature in {city} is {temperature}°C with {description}. "
+                              f"Humidity is {humidity}%.")
+            speak(weather_report)
+            print(weather_report)
+        else:
+            speak("City not found. Please try again with a different city.")
+    except Exception as e:
+        speak("Sorry, I couldn't fetch the weather information. Please check your internet connection.")
+        print(e)
+
+def tellAJoke():
+    """Tell a random joke."""
+    jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "What do you call fake spaghetti? An impasta!",
+        "Why don’t skeletons fight each other? They don’t have the guts.",
+        "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+        "Why did the math book look sad? Because it had too many problems."
+    ]
+    joke = random.choice(jokes)
+    speak(joke)
+    print(joke)
+
+def funResponses(query):
+    """Provide fun responses based on user input."""
+    if "how are you" in query:
+        response = "I'm doing great, thanks for asking! How about you?"
+    elif "what's up" in query:
+        response = "Not much, just hanging out in the cloud!"
+    elif "tell me a joke" in query:
+        tellAJoke()
+        return  # Exit to avoid duplicate responses
+    else:
+        response = "I'm just here to help. What can I do for you?"
+
+    speak(response)
+    print(response)
+
 
 if __name__ == '__main__':
 
-    speak(f"Hey,{greetMe()} I am AURA. How may I help you today")
+    speak(f"Hey, {greetMe()} I am AURA. How may I help you today")
 
     while True:
         x = takeCommand().lower()
 
-        if "open youtube" in x:
-            engine.say("Opening Youtube")
-            print("Opening Youtube...")
-            webbrowser.open("https://www.youtube.com/")
+        # List of websites to open
+        sites = [['youtube', 'https://www.youtube.com/'], ['open google', 'https://google.com/']]
 
-        if "open google" in x:
-            engine.say("Opening Google")
-            print("Opening Google...")
-            webbrowser.open("https://google.com/")
+        # Open websites based on voice command
+        for site in sites:
+            if site[0] in x:
+                print(f"Opening {site[0]}...")
+                speak(f"Opening {site[0]}")
+                webbrowser.open(site[1])
 
+        # Tell the current time
+        if 'time' in x:
+            time = datetime.now().strftime('%H:%M')
+            speak(f"The time is {time}")
+            print(f"The time is {time}")
+
+        # Exit the program when 'bye' is mentioned
         if "bye" in x:
             print("Bye Bye friend!")
-            engine.say("Bye Bye Friend")
-            engine.runAndWait()
+            speak("Bye Bye Friend")
             break
-        else:
-            engine.say(x)
-            engine.runAndWait()
 
-'''
+        # Handle other commands
+        else:
+            speak(x)
+
+        if "search google" in x:
+            speak("What would you like to search for on Google?")
+            search_query = takeCommand().lower()
+            searchGoogle(search_query)
+
+        # Wikipedia search functionality
         if 'wikipedia' in x:
             speak("Searching Wikipedia...")
-            engine.say("What do you want to search")
-            engine.runAndWait()
-            x = takeCommand().lower
-            results = wikipedia.summary(x, sentences=5)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-'''
+            speak("What do you want to search?")
+            query = takeCommand().lower()  # Correctly call takeCommand()
+            if query != "None":  # Check if the query is not empty
+                results = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+
+        funResponses()
